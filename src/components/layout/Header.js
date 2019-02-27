81f64116a70b3910eec78bb216b0ece92a81b958
Kabir Gogoi <kabirgogoi9@gmail.com>
2019-02-27 23:15:26 +0530

    remove link from branding

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,9 +8,10 @@ const Header = props => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-info mb-3 py-0">
       <div className="container">
-        <a href="#!" className="navbar-brand">
+        <Link to="/" className="navbar-brand">
           <h3>{branding}</h3>
-        </a>
+        </Link>
+
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
