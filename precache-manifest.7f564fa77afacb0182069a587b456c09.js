237711ce61dc439e7bbf2913f0093e8cb1000ce1
Kabir Gogoi <kabirgogoi9@gmail.com>
2019-02-27 22:59:27 +0530

    Updates

---
+++ precache-manifest.7f564fa77afacb0182069a587b456c09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "ede0628c6c4f1506e41f",
+    "url": "/contactmanager/static/js/main.ede0628c.chunk.js"
+  },
+  {
+    "revision": "5ce728d5b88198765058",
+    "url": "/contactmanager/static/js/1.5ce728d5.chunk.js"
+  },
+  {
+    "revision": "ede0628c6c4f1506e41f",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "5ce728d5b88198765058",
+    "url": "/contactmanager/static/css/1.4a362337.chunk.css"
+  },
+  {
+    "revision": "e847f7a5e77f59ee38af0b45efa20f82",
+    "url": "/contactmanager/index.html"
+  }
+];
