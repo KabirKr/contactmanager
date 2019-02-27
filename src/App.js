254e1914e5e654f63782098780598a5cba43ec42
Kabir Gogoi <kabirgogoi9@gmail.com>
2019-02-27 19:15:34 +0530

    react lifecycle methods

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contacts from "./components/contacts/Contacts";
 import Header from "./components/layout/Header";
 import About from "./components/Pages/About";
 import NotFound from "./components/Pages/NotFound";
+import Test from "./components/Test/Test";
 
 import { Provider } from "./context";
 
@@ -24,6 +25,7 @@ class App extends Component {
                 <Route exact path="/contact/add" component={AddContact} />
                 {/* <Route exact path="/about/:id" component={About} /> */}
                 <Route exact path="/about" component={About} />
+                <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
               </Switch>
             </div>
